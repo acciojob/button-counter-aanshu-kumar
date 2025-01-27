acea331c678ec5e8762b73b99d5420c0c3e1c526
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-27 12:34:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
         <p>Button clicked {click} times.</p>
-        <button onClick={()=>setClick(click++)}>Click me</button>
+        <button onClick={()=>setClick(click+1)}>Click me</button>
     </div>
   )
 }
